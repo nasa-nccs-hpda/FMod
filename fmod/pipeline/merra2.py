76786ab572719db2dd052a6fd3195a778c586127
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-01-18 15:45:07 -0700

    Implement dali pipeline

--- fmod/pipeline/merra2.py
+++ fmod/pipeline/merra2.py
@@ -55,8 +55,7 @@ class MERRA2InputIterator(object):
             self.fmbatch.load( next_date )
             self.current_date = next_date
         train_data: xa.Dataset = self.fmbatch.get_train_data( self.get_day_offset() )
-        task_config = dict(cfg().task)
-        (inputs, targets, forcings) = batch.extract_inputs_targets_forcings(train_data, target_lead_times=self.target_lead_times, **task_config )
+        (inputs, targets, forcings) = batch.extract_inputs_targets_forcings(train_data, target_lead_times=self.target_lead_times )
         self.i = (self.i + 1) % self.length
         return inputs, targets, forcings
 
