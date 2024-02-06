bc5d1c2bf93eafb7151f585c1c347a7edbdbd6d8
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-02-06 10:40:37 -0700

    Implement sfno pipeline

--- fmod/base/util/model.py
+++ fmod/base/util/model.py
@@ -594,12 +594,11 @@ def variable_to_stacked( vname: str,  variable: xarray.Variable, sizes: Mapping[
     An xarray.Variable with dimensions preserved_dims + ("channels",).
   """
   stack_to_channels_dims = [ d for d in variable.dims if d not in preserved_dims]
-  print(f"variable_to_stacked {vname}{variable.dims}: stack={stack_to_channels_dims} preserve={preserved_dims}")
+  dims = {dim: variable.sizes.get(dim) or sizes[dim] for dim in preserved_dims}
+  print( f"Variable {vname}{dims}: stack to channels {[ f'{d}[{variable.sizes.get(d,sizes[d])}]' for d in stack_to_channels_dims]}")
   if stack_to_channels_dims:
     variable = variable.stack(channels=stack_to_channels_dims)
-  dims = {dim: variable.sizes.get(dim) or sizes[dim] for dim in preserved_dims}
   dims["channels"] = variable.sizes.get("channels", 1)
-  print( f"Variable {vname}{dims}: stack to channels {[ f'{d}[{variable.sizes.get(d,sizes[d])}]' for d in stack_to_channels_dims]}")
   return variable.set_dims(dims)
 
 
