d98a8103710947bd052196d08ab2dbc73818021c
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-02-07 12:18:05 -0700

    Implement sfno pipeline

--- fmod/plot/training_results.py
+++ fmod/plot/training_results.py
@@ -88,7 +88,7 @@ class ResultsPlotter:
 			ax = self.axs[ip]
 			timeslce: Tensor = pdata[self.istep]
 			ax.set_title(f"{self.ptypes[ip]}")
-			image_data: Tensor = timeslce[self.ichannel]
+			image_data: Tensor = timeslce[0,self.ichannel] if (timeslce.dim()==4) else timeslce[self.ichannel]
 			print(f"plot[{self.ptypes[ip]}]({self.istep},{self.ichannel}): timeslce{list(timeslce.shape)} image_data{list(image_data.shape)}")
 			if ip == 0: self.vrange = self.gridops.color_range(image_data, 2.0)
 			plot_args = dict( cmap=cmap, origin=origin, vmin=self.vrange[0], vmax=self.vrange[1], **kwargs )
