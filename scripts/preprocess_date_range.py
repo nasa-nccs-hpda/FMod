b0df7cc299666c4daeeb7fc084e57d8c6d498979
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-02-16 17:29:36 -0700

    Implement sres pipeline

--- scripts/preprocess_date_range.py
+++ scripts/preprocess_date_range.py
@@ -11,9 +11,9 @@ from multiprocessing import Pool, cpu_count
 hydra.initialize( version_base=None, config_path="../config" )
 configure( 'merra2-sr' )
 reprocess=True
-nproc = 1 # cpu_count()-2
-start: date = date(1990,4,1)
-end: date = date(1990,4,2)
+nproc = cpu_count()-2
+start: date = date(1990,1,1)
+end: date = date(1991,1,1)
 
 def process( d: date ) -> Dict[str,StatsAccumulator]:
 	reader = MERRA2DataProcessor()
