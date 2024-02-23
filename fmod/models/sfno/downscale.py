4c95fce24c9d30633938d27c879fdbced3bfdede
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-02-23 16:13:18 -0700

    Implement sres pipeline

--- fmod/models/sfno/downscale.py
+++ fmod/models/sfno/downscale.py
@@ -2,27 +2,28 @@ import xarray as xa
 import numpy as np
 from fmod.base.util.config import cfg
 from fmod.models.sfno import sht
+from typing import List, Union, Tuple, Optional, Dict, Type
 import torch
 
 class SFNODownscaler(object):
 
-	def __init__(self, target: xa.DataArray, **kwargs):
+	def __init__(self, target: xa.DataArray, source_shape: List[int]=None, **kwargs):
 		self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 		self.c = cfg().task.coords
 		self.coords = target.coords
 		self.dims = target.dims
 		self.attrs = target.attrs
 		self.tname = target.name
-		self.nlat, self.nlon = target.sizes[self.c['y']], target.sizes[self.c['x']]
-		self.n_theta, self.n_lambda = self.nlat, self.nlon
-		self.sht = sht.RealSHT(self.n_theta, self.n_lambda, grid="equiangular").to(self.device)
-		self.isht = sht.InverseRealSHT(self.nlat, self.nlon, grid="equiangular").to(self.device)
+		self.target_shape = target.sizes[self.c['y']], target.sizes[self.c['x']]      # lat, lon6
+		self.source_shape = self.target_shape if source_shape is None else source_shape    # lat, lon
+		self.sht = sht.RealSHT( *self.source_shape, *self.target_shape, grid="equiangular").to(self.device)
+		self.isht = sht.InverseRealSHT( *self.target_shape, *self.target_shape, grid="equiangular").to(self.device)
 		self.coef: torch.Tensor = None
 
 
 	def process( self, variable: xa.DataArray ) -> xa.DataArray:
 		signal: torch.Tensor = torch.from_numpy(variable.values).to(self.device)
-		print( f"SFNO: signal.shape={signal.shape}, nlat,nlon={(self.nlat, self.nlon)}, n_theta,n_lambda={(self.n_theta,self.n_lambda)}" )
+		print( f"SFNO: signal.shape={signal.shape}, source_shape={self.source_shape}, target_shape={self.target_shape}" )
 		self.coef = self.sht(signal)
 		print(f" ---> coef.shape={self.coef.shape}")
 		downscaled: np.ndarray = self.isht( self.coef ).numpy()
