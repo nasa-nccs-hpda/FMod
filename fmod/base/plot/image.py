c71cb44dd33cac10f875026ddbe0e2555225ed66
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-03-06 16:03:15 -0700

    Implement sres pipeline

--- fmod/base/plot/image.py
+++ fmod/base/plot/image.py
@@ -114,6 +114,86 @@ def mplplot( target: xa.Dataset, vnames: List[str],  task_spec: Dict, **kwargs )
 	return ipw.VBox([tslider, lslider, fig.canvas])
 
 @exception_handled
+def mplplot( target: xa.Dataset, vnames: List[str],  task_spec: Dict, **kwargs ):
+	ims, pvars, nvars, ptypes = {}, {}, len(vnames), ['']
+	forecast: Optional[xa.Dataset] = kwargs.pop('forecast',None)
+	time: xa.DataArray = xaformat_timedeltas( target.coords['time'] )
+	levels: xa.DataArray = target.coords['level']
+	lunits : str = levels.attrs.get('units','')
+	dayf = 24/task_spec['data_timestep']
+	target.assign_coords( time=time )
+	if forecast is not None:
+		forecast.assign_coords(time=time)
+		ptypes = ['target', 'forecast', 'difference']
+	ncols =  len( ptypes )
+	lslider: ipw.IntSlider = ipw.IntSlider( value=0, min=0, max=levels.size-1, description='Level Index:', )
+	tslider: ipw.IntSlider = ipw.IntSlider( value=0, min=0, max=time.size-1, description='Time Index:', )
+	print_data_column( target, vnames[0], **kwargs )
+	errors: Dict[str,xa.DataArray] = {}
+
+	with plt.ioff():
+		fig, axs = plt.subplots(nrows=nvars, ncols=ncols, sharex=True, sharey=True, figsize=[ncols*5, nvars*3], layout="tight")
+
+	for iv, vname in enumerate(vnames):
+		tvar: xa.DataArray = normalize(target,vname,**kwargs)
+		plotvars = [ tvar ]
+		if forecast is not None:
+			fvar: xa.DataArray = normalize(forecast,vname,**kwargs)
+			diff: xa.DataArray = tvar - fvar
+			errors[vname] = rmse(diff)
+			plotvars = plotvars + [ fvar, diff ]
+		vrange = None
+		for it, pvar in enumerate( plotvars ):
+			ax = axs[ iv ] if ncols == 1 else axs[ iv, it ]
+			ax.set_aspect(0.5)
+			if it != 1: vrange = cscale( pvar, 2.0 )
+			tslice: xa.DataArray = pvar.isel(time=tslider.value)
+			if "level" in tslice.dims:
+				tslice = tslice.isel(level=lslider.value)
+			ims[(iv,it)] =  tslice.plot.imshow( ax=ax, x="lon", y="lat", cmap='jet', yincrease=True, vmin=vrange[0], vmax=vrange[1]  )
+			lgm().log(f" >> Create image {vname} {ptypes[it]}:  nnan={nnan(tslice.values)}")
+			pvars[(iv,it)] =  pvar
+			ax.set_title(f"{vname} {ptypes[it]}")
+
+	@exception_handled
+	def time_update(change):
+		sindex = change['new']
+		lindex = lslider.value
+		fig.suptitle(f'Forecast day {sindex/dayf:.1f}, Level: {levels.values[lindex]:.1f} {lunits}', fontsize=10, va="top", y=1.0)
+		lgm().log( f"time_update: tindex={sindex}, lindex={lindex}, vnames={vnames}, ncols={ncols}")
+		for iv1, vname1 in enumerate(vnames):
+			for it1 in range(ncols):
+				ax1 = axs[ iv ] if ncols == 1 else axs[ iv, it ]
+				im1, dvar1 = ims[ (iv1, it1) ], pvars[ (iv1, it1) ]
+				lgm().log(f" >> Time-update {vname1} {ptypes[it1]}: level={lindex}, time={sindex}, shape={dvar1.shape}, nnan={nnan(dvar1.values)}")
+				tslice1: xa.DataArray =  dvar1.isel( level=lindex, time=sindex, drop=True, missing_dims="ignore")
+				im1.set_data( tslice1.values )
+				ax1.set_title(f"{vname1} {ptypes[it1]}")
+
+		fig.canvas.draw_idle()
+
+	@exception_handled
+	def level_update(change):
+		lindex = change['new']
+		tindex = tslider.value
+		fig.suptitle(f'Forecast day {tindex/dayf:.1f}, Level: {levels.values[lindex]:.1f} {lunits}', fontsize=10, va="top", y=1.0)
+		lgm().log( f"level_update: lindex={lindex}, tindex={tslider.value}, vnames={vnames}, ncols={ncols}")
+		for iv1, vname1 in enumerate(vnames):
+			for it1 in range(ncols):
+				ax1 = axs[ iv ] if ncols == 1 else axs[ iv, it ]
+				im1, dvar1 = ims[ (iv1, it1) ], pvars[ (iv1, it1) ]
+				lgm().log(f" >> Level-update {vname1} {ptypes[it1]}: level={lindex}, time={tindex}, shape={dvar1.shape}, nnan={nnan(dvar1.values)}")
+				tslice1: xa.DataArray =  dvar1.isel( level=lindex,time=tindex, drop=True, missing_dims="ignore")
+				im1.set_data( tslice1.values )
+				ax1.set_title(f"{vname1} {ptypes[it1]}")
+		fig.canvas.draw_idle()
+
+	tslider.observe( time_update,  names='value' )
+	lslider.observe( level_update, names='value' )
+	fig.suptitle(f' ** Forecast day 0, Level: {levels.values[0]:.1f} {lunits}', fontsize=10, va="top", y=1.0 )
+	return ipw.VBox([tslider, lslider, fig.canvas])
+
+@exception_handled
 def mplplot_error( target: xa.Dataset, forecast: xa.Dataset, vnames: List[str],  **kwargs ):
 	ftime: np.ndarray = xaformat_timedeltas( target.coords['time'], form="day", strf=False ).values
 	tvals = list( range( 1, round(float(ftime[-1]))+1, math.ceil(ftime.size/10) ) )
