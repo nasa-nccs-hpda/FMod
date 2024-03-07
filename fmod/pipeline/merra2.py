2b935eb06f979db02e661684f0aece6ecd0e49af
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-03-06 23:52:47 -0700

    Implement sres pipeline

--- fmod/pipeline/merra2.py
+++ fmod/pipeline/merra2.py
@@ -210,7 +210,7 @@ class MERRA2Dataset(BaseDataset):
         lgm().debug(f" >> >> target variables: {target_variables}")
         target_array: xa.DataArray = ds2array( self.normalize(targets[list(target_variables)]) )
         lgm().debug(f" >> targets{target_array.dims}: {target_array.shape}, nnan={nnan(target_array)}")
-        lgm().debug(f"Extract inputs: basetime= {pd.Timestamp(nptime[0])}, device={cfg().target.device}")
+        lgm().debug(f"Extract inputs: basetime= {pd.Timestamp(nptime[0])}, device={cfg().task.device}")
         self.chanIds['input']  = input_array.attrs['channels']
         self.chanIds['target'] = target_array.attrs['channels']
 
