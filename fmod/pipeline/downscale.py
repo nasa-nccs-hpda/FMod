9d79c8dfe7fde82585120d17cc28f5da315b61d9
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-03-13 14:28:15 -0600

    Implement sres pipeline

--- fmod/pipeline/downscale.py
+++ fmod/pipeline/downscale.py
@@ -3,7 +3,6 @@ import numpy as np
 from fmod.base.util.config import cfg
 from typing import List, Union, Tuple, Optional, Dict, Type, Any, Sequence, Mapping, Literal, Hashable
 import glob, sys, os, time, traceback
-from fmod.pipeline.rescale import QType
 from fmod.models.sfno.downscale.transform import SHTransform
 np.set_printoptions(precision=3, suppress=False, linewidth=150)
 
