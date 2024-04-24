21069c547117b6385620a189e15bd1e63d6b4084
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-04-24 12:55:03 -0600

    Integrate SRDN model

--- tests/batch_reader_test.py
+++ tests/batch_reader_test.py
@@ -5,19 +5,22 @@ from fmod.base.util.dates import date_range, cfg_date_range
 from fmod.base.util.config import configure, cfg, start_date,  cfg2args, pp
 from fmod.pipeline.ncbatch import ncBatchDataset
 from fmod.base.util.logging import lgm, exception_handled, log_timing
-from fmod.base.util.ops import pctnan
 
 hydra.initialize(version_base=None, config_path="../config")
 configure('merra2-unet-s1')
-cfg().task.device = "cpu"
 lgm().set_level( logging.DEBUG )
 
-dataset = ncBatchDataset( cfg().task, vres="low" )
-data_iter = iter(dataset)
+# set device
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+if torch.cuda.is_available():
+	torch.cuda.set_device(device.index)
 
-for inp, tar in data_iter:
-	print(f" ** inp shape={inp.shape}, pct-nan= {pctnan(inp)}")
-	print(f" ** tar shape={tar.shape}, pct-nan= {pctnan(tar)}")
+input_dataset  = ncBatchDataset( cfg().task, load_inputs=True, load_base=False, load_targets=True )
+results = next(iter(input_dataset))
+sample_input = results['input']
+print( f"sample_input{sample_input.dims}: {sample_input.shape} {list(sample_input.coords.keys())}" )
+for k,c in sample_input.coords.items():
+	print( f"{k}{c.shape}: {c[0]:.2f} -> {c[-1]:.2f}")
 	break
 
 
