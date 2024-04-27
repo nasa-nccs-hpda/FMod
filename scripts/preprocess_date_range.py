24541e9989a089ccceb53c2872a2e93d0b2247ee
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-04-27 13:30:58 -0600

    Integrate UNET model

--- scripts/preprocess_date_range.py
+++ scripts/preprocess_date_range.py
@@ -12,8 +12,8 @@ hydra.initialize( version_base=None, config_path="../config" )
 configure( 'merra2-sr' )
 reprocess=False
 nproc = cpu_count()
-start: date = date(1996,1,1 )
-end: date = date(1996,1,8 )
+start: date = date(1996,2,1 )
+end: date = date(1996,2,30 )
 
 def process( d: date ) -> Dict[str,StatsAccumulator]:
 	reader = MERRA2DataProcessor()
