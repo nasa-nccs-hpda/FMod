7f0d71bf8d5cdb5d396ba75ce63d1764f43d7fa2
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-05-10 13:45:52 -0600

    Add MSCNN

--- fmod/models/sres/mscnn/network.py
+++ fmod/models/sres/mscnn/network.py
@@ -12,12 +12,12 @@ class DoubleConv(nn.Module):
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding='same', bias=False),
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding='same', bias=True),
             nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding='same', bias=False),
+            nn.LeakyReLU(negative_slope = 0.1),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding='same', bias=True),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            nn.LeakyReLU(negative_slope = 0.1),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -37,34 +37,6 @@ class Down(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         y = self.maxpool_conv(x)
         return y
-
-
-class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels: int, out_channels: int, bilinear: bool=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv( in_channels, out_channels )
-
-    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
-        x1 = self.up(x1)
-        # input is CHW
-        diffy: int = x2.size()[2] - x1.size()[2]
-        diffx: int = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffx // 2, diffx - diffx // 2,  diffy // 2, diffy - diffy // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x: torch.Tensor = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
 
 class Upscale(nn.Module):
     """Upscaling then double conv"""
