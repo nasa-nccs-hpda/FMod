70091dce43b3a116944f6ef1c06a8bc43f6d03d7
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-05-19 16:18:16 -0600

    Refactor for s3export

--- fmod/model/sres/manager.py
+++ fmod/model/sres/manager.py
@@ -26,7 +26,7 @@ class SRModels:
 		self.model_config['nchannels'] = self.sample_input.shape[1]
 
 	def get_model(self) -> nn.Module:
-		importpath = f"fmod.models.sres.{self.model_name}.network"
+		importpath = f"fmod.model.sres.{self.model_name}.network"
 		model_package = importlib.import_module(importpath)
 		return model_package.get_model( self.model_config ).to(self.device)
 
