821202de10f69a6b7368abb292b619d6d134cb4d
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-05-19 13:41:36 -0600

    Refactor for s3export

--- fmod/base/util/model-1.py
+++ fmod/base/util/model-1.py
@@ -641,7 +641,7 @@ def dataset_to_stacked( dataset: xarray.Dataset, sizes: Optional[Mapping[str, in
   lgm().debug(f"dataset_to_stacked: {len(dataset.data_vars)} data_vars, preserved_dims={preserved_dims}, concat-list size= {len(data_vars)}")
   coords = { dim: coord for dim, coord in dataset.coords.items() if dim in preserved_dims  }
   stacked_data = xarray.Variable.concat(data_vars, dim="channels")
-  lgm().debug(f"stacked_data{stacked_data.dims}: shape = {stacked_data.shape}, coords={list(coords.keys())}")
+  lgm().log(f" %%%% stacked_data{stacked_data.dims}: shape = {stacked_data.shape}, coords={list(coords.keys())}",display=True)
   return xarray.DataArray( data=stacked_data.values, dims=stacked_data.dims, coords=coords )
 
 
