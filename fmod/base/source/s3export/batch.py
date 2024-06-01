f4359966f71862e0dea424cd7b87c4f99e24311e
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-06-01 12:28:57 -0600

    Update pipeline & workflow.

--- fmod/base/source/s3export/batch.py
+++ fmod/base/source/s3export/batch.py
@@ -38,12 +38,12 @@ def datelist( date_range: Tuple[datetime, datetime] ) -> pd.DatetimeIndex:
 
 class S3ExportDataLoader(SRDataLoader):
 
-	def __init__(self, task_config: DictConfig, tile_size: Dict[str, int], vres: srRes, **kwargs):
+	def __init__(self, task_config: DictConfig, vres: srRes, **kwargs):
 		SRDataLoader.__init__(self, task_config, vres )
 		self.coords_dataset: xa.Dataset = xa.open_dataset( coords_filepath(), **kwargs)
 #		self.xyc: Dict[str,xa.DataArray] = { c: self.coords_dataset.data_vars[ self.task.coords[c] ] for c in ['x','y'] }
 #		self.ijc: Dict[str,np.ndarray]   = { c: self.coords_dataset.coords['i'].values.astype(np.int64) for c in ['i','j'] }
-		self.tile_size: Dict[str,int] = tile_size
+		self.tile_size: Dict[str,int] = cfg().task.tile_size
 		self.varnames: Dict[str, str] = self.task.input_variables
 		self.use_memmap = task_config.get('use_memmap', False)
 		self.shape = None
@@ -56,6 +56,13 @@ class S3ExportDataLoader(SRDataLoader):
 		tile_bnds = [ origin['y'], origin['y'] + self.tile_size['y'], origin['x'], origin['x'] + self.tile_size['x'] ]
 		if idx == 0: lgm().log( f"     ------------------>> cut_tile: origin={origin}, tile_bnds = {tile_bnds}")
 		return data_grid[ tile_bnds[0]: tile_bnds[1], tile_bnds[2]: tile_bnds[3] ]
+
+	def cut_domain( self, timeslice_data: np.ndarray ):
+		origin: Dict[str,int] = cfg().task.origin
+		tile_grid: Dict[str,int] = cfg().task.tile_grid
+		tile_bnds = { c:  [origin[c], origin[c]+self.tile_size[c]*tile_grid[c]] for c in ['x','y'] }
+		lgm().log( f"     ------------------>> cut_domain: origin={origin}, tile_bnds = {tile_bnds}")
+		return timeslice_data[ tile_bnds['y'][0]:tile_bnds['y'][1], tile_bnds['x'][0]:tile_bnds['x'][1] ]
 
 	# def cut_xy_coords(self, oindx: Dict[str,int] )-> Dict[str,xa.DataArray]:
 	# 	tcoords: Dict[str,np.ndarray] = { c:  self.cut_coord( origin, c ) for idx, c in enumerate(['i','j']) }
@@ -73,6 +80,12 @@ class S3ExportDataLoader(SRDataLoader):
 			print( f"Loaded {vid}({date.strftime('%H:%d/%m/%Y')}): shape={raw_data.shape}")
 			self.shape = list( raw_data.shape )
 		return raw_data
+
+	def load_global_timeslice(self, vid: str, date: datetime) -> np.ndarray:
+		fpath = data_filepath(vid, date, self.vres)
+		mmap_mode = 'r' if self.use_memmap else None
+		timeslice: np.memmap = np.load(fpath, allow_pickle=True, mmap_mode=mmap_mode)
+		return self.cut_domain(timeslice)
 
 	def load_channel( self, idx: int, origin: Dict[str,int], vid: Tuple[str,str], date: datetime ) -> xa.DataArray:
 		raw_data: np.memmap = self.open_timeslice(vid[0], date)
