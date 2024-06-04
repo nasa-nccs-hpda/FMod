73beca0d5309eafaea89049d86ad085de529ac82
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-06-04 17:27:59 -0600

    Update pipeline & workflow.

--- fmod/data/batch.py
+++ fmod/data/batch.py
@@ -91,10 +91,13 @@ class BatchDataset(BaseDataset):
         self.current_batch_data = None
 
     def in_batch(self, time_coord: datetime, batch_date: datetime) -> bool:
-        if time_coord < batch_date: return False
         dt: timedelta = time_coord - batch_date
         hours: int = dt.seconds // 3600
-        return hours < self.hours_per_batch
+        rv = hours < self.hours_per_batch
+        if time_coord < batch_date:
+            rv = None
+        lgm().log( f"in_batch: time_coord={time_coord}, batch_date={batch_date}, hours={hours}, result={rv}")
+        return False if (rv is None) else rv
 
     def get_batch_array(self, oindx: Dict[str,int], batch_date: datetime ) -> xa.DataArray:
         origin = self.scale_coords(oindx)
