85d6d3659198e8780997ec5f2f21bf0c53cb9b12
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-06-11 23:24:15 -0600

    Update pipeline & workflow.

--- scripts/eval_upscale.py
+++ scripts/eval_upscale.py
@@ -15,7 +15,7 @@ dataset = "LLC4320-v1"
 scenario = "s4.1"
 model = "mscnn"
 downscale_factors= [ 2, 2 ]
-ups_mode= 'bicubic'
+ups_mode= 'bilinear'
 
 results = ResultsAccumulator(task,dataset,scenario)
 with ConfigContext(task, model, dataset, scenario) as cc:
