23ad241d2512fbab3815ccea573d4f8c07d4e6bf
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-06-24 12:38:28 -0600

    Update pipeline & workflow.

--- scripts/train-srdn.py
+++ scripts/train-srdn.py
@@ -14,7 +14,7 @@ cname = "sres"
 models = [ 'srdn' ] # [ 'dbpn', 'edsr', 'srdn', 'unet', 'vdsr', 'mscnn', 'lapsrn' ]
 
 ConfigContext.set_defaults(
-	task = "sres",
+	parameters = "cape_basin",
 	dataset = "LLC4320",
 	scenario = "s4",
 	pipeline = "sres",
