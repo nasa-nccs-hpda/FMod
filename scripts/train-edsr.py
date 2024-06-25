abf93c727d663a2c5bb1f2907f0c4056d4f9c3f7
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-06-25 14:32:27 -0600

    Refactor API.

--- scripts/train-edsr.py
+++ scripts/train-edsr.py
@@ -3,7 +3,7 @@ from fmod.controller.workflow import WorkflowController
 
 cname: str = "sres"
 models: List[str] = [ 'edsr' ] # [ 'dbpn', 'edsr', 'srdn', 'unet', 'vdsr', 'mscnn', 'lapsrn' ]
-ccustom: Dict[str,Any] = { 'task.nepochs': 30, 'task.lr': 3e-4, 'pipeline.gpu': 1 }
+ccustom: Dict[str,Any] = { 'task.nepochs': 30, 'task.lr': 1e-4, 'pipeline.gpu': 1 }
 
 configuration = dict(
 	task = "cape_basin",
