690228898f3a28aad5abb99d3dfcab79d83f06b2
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-06-24 20:23:43 -0600

    Update pipeline & workflow.

--- fmod/controller/workflow.py
+++ fmod/controller/workflow.py
@@ -1,8 +1,9 @@
 import torch, time
 from fmod.base.util.config import ConfigContext, cfg
 from fmod.controller.dual_trainer import ModelTrainer
+from fmod.base.io.loader import ncFormat, TSet
 from typing import Any, Dict, List, Tuple, Type, Optional, Union
-from fmod.model.sres.manager import ResultsAccumulator
+from fmod.base.plot.sres import SRPlot
 
 class WorkflowController(object):
 
@@ -10,11 +11,22 @@ class WorkflowController(object):
 		self.cname = cname
 		self.seed = kwargs.get('seed', int( time.time()/60 ) )
 		self.refresh_state = kwargs.get('refresh_state', False )
+		self.config: ConfigContext = None
+		self.trainer: ModelTrainer = None
 		ConfigContext.set_defaults( **configuration )
 
 	def train(self, models: List[str], **kwargs):
 		for model in models:
 			with ConfigContext(self.cname, model=model, **kwargs) as cc:
-				trainer: ModelTrainer = ModelTrainer(cc)
-				trainer.train(refresh_state=self.refresh_state, seed=self.seed)
+				self.config = cc
+				self.trainer = ModelTrainer(cc)
+				self.trainer.train(refresh_state=self.refresh_state, seed=self.seed)
+
+	def plot_init(self, cname, **kwargs ):
+		self.config = ConfigContext.activate_global(cname, **kwargs )
+		self.trainer = ModelTrainer( self.config )
+
+	def srplot(self, tset: TSet, **kwargs ):
+		srplot = SRPlot(self.trainer, tset, **kwargs )
+		return srplot.plot()
 
