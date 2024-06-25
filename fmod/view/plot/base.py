1b8a43a28d7f05f650d69911155c673bc915801c
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-06-25 11:59:18 -0600

    Update pipeline & workflow.

--- fmod/view/plot/base.py
+++ fmod/view/plot/base.py
@@ -1,5 +1,6 @@
 from fmod.controller.dual_trainer import ModelTrainer
 import matplotlib.pyplot as plt
+from typing import Any, Dict, List, Tuple, Type, Optional, Callable
 
 class Plot:
 
@@ -8,7 +9,7 @@ class Plot:
 		self.fsize = kwargs.get('fsize', 8.0)
 		self.yscale = kwargs.get('yscale', 'log' )
 		self.fig = None
-		self.ax = None
+		self.axs = None
 		self.aspect = 1
 
 	def create_figure(self, title: str, **kwargs):
@@ -18,7 +19,7 @@ class Plot:
 		ncols  = kwargs.get('ncols', 1)
 		callbacks: Dict[str,Callable] = kwargs.get( 'callbacks', {} )
 		with plt.ioff():
-			self.fig, self.ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=[self.fsize*self.aspect, self.fsize], sharex=sharex, sharey=sharey, layout="tight")
+			self.fig, self.axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=[self.fsize*self.aspect, self.fsize], sharex=sharex, sharey=sharey, layout="tight")
 			self.fig.suptitle( title, fontsize=14, va="top", y=1.0)
 			for event, callback in callbacks.items():
 				self.fig.canvas.mpl_connect(event,callback)
