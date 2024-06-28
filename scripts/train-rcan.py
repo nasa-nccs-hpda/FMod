c78b3abd76c745b618ba475e306eabbab2c8e303
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-06-28 16:24:24 -0600

    Refactor Model defs

---
+++ scripts/train-rcan.py
@@ -0,0 +1,23 @@
+from typing import Any, Dict, List, Tuple, Type, Optional, Union
+from fmod.controller.workflow import WorkflowController
+
+cname: str = "sres"
+models: List[str] = [ 'rcan' ]
+ccustom: Dict[str,Any] = { 'task.nepochs': 5, 'task.lr': 1e-4 }
+
+configuration = dict(
+	task = "cape_basin_1x1",
+	dataset = "LLC4320",
+	pipeline = "sres",
+	platform = "explore"
+)
+
+controller = WorkflowController( cname, configuration )
+controller.train( models, **ccustom )
+
+
+
+
+
+
+
