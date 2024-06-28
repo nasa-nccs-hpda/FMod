33dd89b0613a5b20936cf051f89425aadcb7ff81
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-06-28 17:23:39 -0600

    Refactor Model defs

--- scripts/train-vdsr.py
+++ scripts/train-vdsr.py
@@ -4,7 +4,7 @@ from fmod.controller.workflow import WorkflowController
 cname: str = "sres"
 models: List[str] = [ 'vdsr' ]
 ccustom: Dict[str,Any] = { 'task.nepochs': 5, 'task.lr': 1e-4 }
-refresh = True
+refresh = False
 
 configuration = dict(
 	task = "cape_basin_1x1",
