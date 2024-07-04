d7eb375f1ecb8a0b03b0c42d81d26a492e09aa1b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-07-04 13:09:30 -0600

    Add support for swot dataset

--- fmod/base/source/swot/raw.py
+++ fmod/base/source/swot/raw.py
@@ -8,6 +8,7 @@ from glob import glob
 from typing import Any, Mapping, Sequence, Tuple, Union, List, Dict, Literal, Optional
 from fmod.base.io.loader import data_suffix, path_suffix
 from fmod.base.util.logging import lgm, exception_handled, log_timing
+from .util import mds2d
 import numpy as np
 
 def filepath(ftype: str ) -> str:
@@ -26,4 +27,7 @@ class SWOTRawDataLoader(SRRawDataLoader):
 		mask = var_template == 0
 		var_template[~mask] = var_data
 		var_template[mask] = np.nan
-		return var_template
+		sss_east, sss_west = mds2d(var_template)
+		print(sss_east.shape, sss_west.shape)
+		result = np.c_[sss_east, sss_west.T[::-1, :]]
+		return result
