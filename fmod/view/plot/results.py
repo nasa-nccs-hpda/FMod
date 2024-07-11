fcba1984e387539535d18ae4a93ee2617eb64b46
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-07-11 13:21:33 -0600

    Add support for swot dataset

--- fmod/view/plot/results.py
+++ fmod/view/plot/results.py
@@ -97,8 +97,8 @@ class ResultPlot(Plot):
 		print( f"target_data shape = {target_data.shape}")
 		print(f"product_data shape = {product_data.shape}")
 		model_input: xa.DataArray = xa.DataArray( input_data, dims=['batch','channels','y','x'] )
-		target: xa.DataArray = to_xa(self.sample_target, target_data )
-		prediction: xa.DataArray = to_xa(self.sample_target, product_data)
+		target: xa.DataArray = xa.DataArray( target_data, dims=['batch','channels','y','x'] )
+		prediction: xa.DataArray = xa.DataArray( product_data, dims=['batch','channels','y','x'] )
 		domain: xa.DataArray = self.trainer.target_dataset(self.tset).load_global_timeslice(index=0)
 		lgm().log( f"update_tile_data{self.tile_index}: prediction shape = {prediction.shape}, target shape = {target.shape}")
 
