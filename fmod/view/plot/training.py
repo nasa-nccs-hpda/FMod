7bcb0f24d1f11f5fb21947b0aa2f11e0ad01b3e4
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-07-17 14:15:08 -0600

    Add support for swot dataset

--- fmod/view/plot/training.py
+++ fmod/view/plot/training.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from fmod.view.plot.base import Plot
 import ipywidgets as ipw
+from fmod.base.util.logging import lgm, exception_handled, log_timing
 from fmod.base.io.loader import ncFormat, TSet
 from fmod.controller.dual_trainer import ModelTrainer
 
@@ -13,12 +14,14 @@ class TrainingPlot(Plot):
 		self.min_loss = {}
 		self.create_figure( title='Training Loss' )
 
+	@exception_handled
 	def plot(self) -> ipw.Box:
 		(x, y) = self.trainer.results_accum.get_plot_data()
 		for tset in [TSet.Train, TSet.Validation]:
 			xp, yp = x[tset], y[tset]
 			self.min_loss[tset] = yp.min() if (yp.size > 0) else 0.0
 			self.axs.plot(xp, yp, self.fmt[tset], label=tset.name)
+			print( f"Plotting {xp.size} {tset.name} points" )
 		self.axs.set_xlabel("Epoch")
 		self.axs.set_ylabel("Loss")
 		self.axs.set_yscale(self.yscale)
