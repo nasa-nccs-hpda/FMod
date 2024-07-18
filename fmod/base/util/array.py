ee2a8054682724c241e1b5e197bc726496a44dfb
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-07-18 14:42:12 -0600

    Add support for swot dataset

--- fmod/base/util/array.py
+++ fmod/base/util/array.py
@@ -91,9 +91,9 @@ def upsample( input_tensor: Tensor ) -> Tensor:
 
 def xa_upsample(input_array: xa.DataArray, **kwargs) -> xa.DataArray:
     coords = kwargs.get( 'coords', UPSAMPLE_COORDS )
-    upsampled =  input_array.interp( coords=coords, method=cfg().task.upsample_mode, assume_sorted=True)
     csize = { cn:cv.shape for cn,cv in coords.items() }
-    print( f"xa_upsample, input{input_array.shape}, upsampled{upsampled.shape}, coords{csize}")
+    print( f"xa_upsample, input{input_array.shape}, coords{csize}")
+    upsampled = input_array.interp(coords=coords, method=cfg().task.upsample_mode, assume_sorted=True)
     return upsampled
 
     #target_channels = cfg().task.target_variables
