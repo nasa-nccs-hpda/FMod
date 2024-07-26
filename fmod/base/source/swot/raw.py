d5385a740686a33629f177a912847aa794e21016
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-07-26 17:31:41 -0600

    Add support for swot dataset

--- fmod/base/source/swot/raw.py
+++ fmod/base/source/swot/raw.py
@@ -160,7 +160,7 @@ class SWOTRawDataLoader(SRRawDataLoader):
 			return self.norm( batch )
 
 	def norm(self, batch: xa.DataArray ) -> xa.DataArray:
-		print( f"norm({batch.name}): batch{batch.dims}{batch.shape}")
+		print( f"norm({batch.name}): batch{batch.dims}{batch.shape}, channels={batch.coords['channels'].values.tolist()}")
 		ntype: str = cfg().task.norm
 		if ntype == 'lnorm':
 			bmean, bstd = batch.mean(dim=["x", "y"], skipna=True, keep_attrs=True), batch.std(dim=["x", "y"], skipna=True, keep_attrs=True)
