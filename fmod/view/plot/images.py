00e99f82ab1bf7b106dc060a55d3c86eebd4f2ea
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-08-11 17:47:43 -0600

    Add support for swot dataset

--- fmod/view/plot/images.py
+++ fmod/view/plot/images.py
@@ -134,9 +134,8 @@ class ResultImagePlot(Plot):
 		ax.set_ylim([0, 100])
 		ptype: str = self.plot_titles[iplot]
 		image: xa.DataArray = self.images_data[ptype]
-		vrange = cscale(image, 2.0)
-		print( f"subplot_image[{ptype}]: image{image.dims}{image.shape}, vrange={vrange}")
-		iplot: AxesImage =  image.plot.imshow(ax=ax, x="x", y="y", cmap='jet', yincrease=True, vmin=vrange[0], vmax=vrange[1])
+		print( f"subplot_image[{ptype}]: image{image.dims}{image.shape}")
+		iplot: AxesImage =  image.plot.imshow(ax=ax, x="x", y="y", cmap='jet', yincrease=True, vmin=-1.5, vmax=1.5 )
 		iplot.colorbar.remove()
 		ax.set_title( self.get_subplot_title(ptype) )
 		self.ims[ iplot ] = iplot
